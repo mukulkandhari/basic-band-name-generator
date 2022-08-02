bd701d6feecaad20e2951c0967c2a2a5267dfa01
Mukul <103566649+mukulkandhari@users.noreply.github.com>
2022-08-02 22:13:29 +0530

    Added the Python file - the code

---
+++ main (1).py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("What's up y'all?")
+#2. Ask the user for the city that they grew up in.
+city_name = input("What city did you grow up in?\n")
+#3. Ask the user for the name of a pet.
+pet_name = input("What's the name of your pet?\n") 
+#4. Combine the name of their city and pet and show them their band name.
+print(city_name + " " + pet_name)
